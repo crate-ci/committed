0c7b80dff6f3c356147a04ef2727bae95c9afdf3
Ed Page <eopage@gmail.com>
2019-08-01 10:39:09 -0600

    fix(git): Don't assume range from single commit
    Be more git-like

--- src/git.rs
+++ src/git.rs
@@ -1,71 +1,81 @@
 pub struct RevSpec<'r> {
     from: git2::Commit<'r>,
-    to: git2::Commit<'r>,
+    to: Option<git2::Commit<'r>>,
 }
 
 impl<'r> RevSpec<'r> {
     pub fn parse(repo: &'r git2::Repository, revspec: &str) -> Result<Self, failure::Error> {
         let commits = repo.revparse(revspec)?;
         let from = commits.from().unwrap().as_commit().unwrap().clone();
-        let to = commits
-            .to()
-            .map(|o| o.as_commit().unwrap().clone())
-            .unwrap_or_else(|| {
-                let id = repo.refname_to_id("HEAD").unwrap();
-                repo.find_commit(id).unwrap()
-            });
-        let merged_from_id = repo.merge_base(from.id(), to.id())?;
-        if merged_from_id != from.id() {
-            log::debug!(
-                "from/to for revspec {} are on different branches, relying on common parent {}",
-                revspec,
-                merged_from_id
-            );
-        }
-        let merged_from = repo.find_commit(merged_from_id)?;
-        Ok(Self {
-            from: merged_from,
-            to,
-        })
+        let to = commits.to().map(|o| o.as_commit().unwrap().clone());
+        let from = if let Some(ref to) = to.as_ref() {
+            let merged_from_id = repo.merge_base(from.id(), to.id())?;
+            if merged_from_id != from.id() {
+                log::debug!(
+                    "from/to for revspec {} are on different branches, relying on common parent {}",
+                    revspec,
+                    merged_from_id
+                );
+            }
+            repo.find_commit(merged_from_id)?
+        } else {
+            from
+        };
+        Ok(Self { from, to })
     }
 
     pub fn iter(&self) -> RevSpecIterator {
-        RevSpecIterator {
-            revspec: &self,
-            needs_to: true,
-            needs_parents: true,
-            parents: self.to.parents(),
+        if let Some(ref to) = self.to.as_ref() {
+            if self.from.id() == to.id() {
+                RevSpecIterator {
+                    revspec: self,
+                    start: None,
+                    parents: None,
+                }
+            } else {
+                RevSpecIterator {
+                    revspec: self,
+                    start: Some(to),
+                    parents: Some(to.parents()),
+                }
+            }
+        } else {
+            RevSpecIterator {
+                revspec: self,
+                start: Some(&self.from),
+                parents: None,
+            }
         }
     }
 }
 
 pub struct RevSpecIterator<'r> {
     revspec: &'r RevSpec<'r>,
-    needs_to: bool,
-    needs_parents: bool,
-    parents: git2::Parents<'r, 'r>,
+    start: Option<&'r git2::Commit<'r>>,
+    parents: Option<git2::Parents<'r, 'r>>,
 }
 
 impl<'r> Iterator for RevSpecIterator<'r> {
     type Item = git2::Commit<'r>;
 
     fn next(&mut self) -> Option<git2::Commit<'r>> {
-        if self.needs_to {
-            if self.revspec.to.id() == self.revspec.from.id() {
-                return None;
-            } else {
-                self.needs_to = false;
-                return Some(self.revspec.to.clone());
-            }
-        } else if self.needs_parents {
-            if let Some(parent) = self.parents.next() {
-                if parent.id() == self.revspec.from.id() {
-                    self.needs_parents = false;
+        if let Some(start) = self.start {
+            self.start = None;
+            Some(start.clone())
+        } else if let Some(parents) = self.parents.as_mut() {
+            if let Some(parent) = parents.next() {
+                if parent.id() != self.revspec.from.id() {
+                    Some(parent)
                 } else {
-                    return Some(parent);
+                    self.parents = None;
+                    None
                 }
+            } else {
+                self.parents = None;
+                None
             }
+        } else {
+            None
         }
-        None
     }
 }

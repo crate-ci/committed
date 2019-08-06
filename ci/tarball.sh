86264b8557fea00435aa92f1345f61dcf923650b
Ed Page <eopage@gmail.com>
2019-08-06 09:43:29 -0500

    feat: Create pre-built binaries
    Fixes #12

---
+++ ci/tarball.sh
@@ -0,0 +1,23 @@
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    cp target/$TARGET/release/cobalt $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main

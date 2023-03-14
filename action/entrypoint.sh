4b737bf8cf1041c208070c2a3f087d554a65540e
Ed Page <eopage@gmail.com>
2023-03-13 20:41:16 -0500

    perf(action): Use composite, rather than docker, action

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -7,19 +7,20 @@ log() {
 }
 
 CMD_NAME="committed"
-args=$(echo "${@:1:$#-1}" | xargs)
-commits="${@: -1}"
 
 if [[ -z $(which ${CMD_NAME} 2>/dev/null) ]]; then
-    log "ERROR: '${CMD_NAME}' not found"
-    exit 1
+    VERSION=1.0.10
+    log "Downloading 'committed' v${VERSION}"
+    wget https://github.com/crate-ci/committed/releases/download/v${VERSION}/committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz
+    sudo tar -xzvf committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz -C /usr/local/bin ./committed
+    rm committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz
 fi
 
 COMMAND="${CMD_NAME}"
 
 echo "Linting commits:"
 git config --global --add safe.directory "$PWD"
-git log --color=always --graph --oneline "${commits}" || true
+git log --color=always --graph --oneline "${INPUT_COMMITS}" || true
 echo ""
 echo "Against 'committed.toml':"
 ${COMMAND} --dump-config - || true
@@ -27,4 +28,4 @@ echo ""
 echo "If this fails, don't sweat it. We're trying to encourage clear communication and not hinder contributions."
 echo "If it is a reasonable issue and you lack time or feel uncomfortable fixing it yourself,"
 echo "let us know and we can mentor or fix it."
-${COMMAND} --color=always $args "${commits}"
+${COMMAND} --color=always $INPUT_ARGS "${INPUT_COMMITS}"

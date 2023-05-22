23b868c7d41f29984171141343da2adff53e7687
Ed Page <eopage@gmail.com>
2023-05-22 13:16:30 -0500

    fix(action): Harden bash script

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -6,17 +6,24 @@ log() {
     echo -e "$1" >&2
 }
 
+_DEFAULT_INSTALL_DIR=${HOME}/bin
+_INSTALL_DIR=${INSTALL_DIR:-${_DEFAULT_INSTALL_DIR}}
 CMD_NAME="committed"
+COMMAND="${_INSTALL_DIR}/${CMD_NAME}"
 
-if [[ -z $(which ${CMD_NAME} 2>/dev/null) ]]; then
+if [[ -z ${INPUT_COMMITS:-} ]]; then
+    log "'INPUT_COMMITS' is required"
+    exit 1
+fi
+
+if [[ ! -x ${COMMAND} ]]; then
     VERSION=1.0.17
     log "Downloading 'committed' v${VERSION}"
     wget https://github.com/crate-ci/committed/releases/download/v${VERSION}/committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz
-    sudo tar -xzvf committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz -C /usr/local/bin ./committed
+    mkdir -p ${_INSTALL_DIR}
+    sudo tar -xzvf committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz -C ${_INSTALL_DIR} ./${CMD_NAME}
     rm committed-v${VERSION}-x86_64-unknown-linux-musl.tar.gz
 fi
-
-COMMAND="${CMD_NAME}"
 
 echo "Linting commits:"
 git config --global --add safe.directory "$PWD"
@@ -28,4 +35,4 @@ echo ""
 echo "If this fails, don't sweat it. We're trying to encourage clear communication and not hinder contributions."
 echo "If it is a reasonable issue and you lack time or feel uncomfortable fixing it yourself,"
 echo "let us know and we can mentor or fix it."
-${COMMAND} --color=always $INPUT_ARGS "${INPUT_COMMITS}"
+${COMMAND} --color=always ${INPUT_ARGS:-} "${INPUT_COMMITS}"

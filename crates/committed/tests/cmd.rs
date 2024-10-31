ed2554531475965725bed37cc9c8eb05cb33e1f7
Ed Page <eopage@gmail.com>
2024-10-31 11:11:48 -0500

    test: Verify WIP behavior

---
+++ crates/committed/tests/cmd.rs
@@ -0,0 +1,25 @@
+use snapbox::str;
+
+#[test]
+fn wip() {
+    let root = snapbox::dir::DirRoot::mutable_temp().unwrap();
+    let root_dir = root.path().unwrap();
+    std::fs::write(root_dir.join("committed.toml"), "").unwrap();
+
+    git2::Repository::init(root_dir).unwrap();
+
+    let message = "WIP: bad times ahead";
+    snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("committed"))
+        .arg("--commit-file=-")
+        .current_dir(root_dir)
+        .stdin(message)
+        .assert()
+        .code(1)
+        .stdout_eq(str![[r#"
+_0.display(): error Work-in-progress commits must be cleaned up
+
+"#]])
+        .stderr_eq(str![]);
+
+    root.close().unwrap();
+}

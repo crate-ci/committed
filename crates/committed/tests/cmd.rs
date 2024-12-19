5249a513d79d393d29218513d7d4760b27a1371c
Ed Page <eopage@gmail.com>
2024-12-18 20:44:57 -0600

    test(cmd): Verify repo behavior

--- crates/committed/tests/cmd.rs
+++ crates/committed/tests/cmd.rs
@@ -2,21 +2,34 @@ use snapbox::str;
 
 #[test]
 fn wip() {
+    let message = "WIP: bad times ahead";
+    let config = "";
+
     let root = snapbox::dir::DirRoot::mutable_temp().unwrap();
     let root_dir = root.path().unwrap();
-    std::fs::write(root_dir.join("committed.toml"), "").unwrap();
+    let config_filename = "committed.toml";
+    let config_path = root_dir.join(config_filename);
+    std::fs::write(&config_path, config).unwrap();
 
-    git2::Repository::init(root_dir).unwrap();
+    let repo = git2::Repository::init(root_dir).unwrap();
+    let mut index = repo.index().unwrap();
+    index
+        .add_path(std::path::Path::new(config_filename))
+        .unwrap();
+    index.write().unwrap();
+    let id = index.write_tree().unwrap();
+    let tree = repo.find_tree(id).unwrap();
+    let sig = repo.signature().unwrap();
+    repo.commit(Some("HEAD"), &sig, &sig, message, &tree, &[])
+        .unwrap();
 
-    let message = "WIP: bad times ahead";
     snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("committed"))
-        .arg("--commit-file=-")
+        .arg("HEAD")
         .current_dir(root_dir)
-        .stdin(message)
         .assert()
         .code(1)
         .stdout_eq(str![[r#"
--: error Work-in-progress commits must be cleaned up
+[..]: error Work-in-progress commits must be cleaned up
 
 "#]])
         .stderr_eq(str![]);
